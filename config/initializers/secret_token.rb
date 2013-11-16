95a2bae3c41913d468849f58cef006f3acdcbb52
Alexander Franke <alexanderfranke1990@gmail.com>
2013-11-16 15:49:18 +0100

    Initial rails project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupdating::Application.config.secret_key_base = '2ff900d6ccef31bc33d5e7e2b2877ac40c2cde11cb0bc68a37debaaae131799ce36e9cd707a95fa309cc2df49318b593331f3ad7087ba6468e03d47e307ac6d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groupdating::Application.config.session_store :cookie_store, key: '_groupdating_session'
